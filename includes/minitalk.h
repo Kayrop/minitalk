89d0206c818be32343bc48b2b55f53eeab854ba9
Kayrop <72454556+Kayrop@users.noreply.github.com>
2024-04-30 18:27:10 +0200

    type changes for config_signals

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: adahmani <adahmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 00:36:47 by kayrop            #+#    #+#             */
-/*   Updated: 2024/04/03 14:52:27 by adahmani         ###   ########.fr       */
+/*   Updated: 2024/04/30 18:23:34 by adahmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@
 
 void	handle_sigusr(int signum, siginfo_t *info, void *ucontext);
 void	bit_send(int pid, char *str, size_t len);
-void	config_signals(void);
 void	send_msg(int sv_pid, char *msg);
 void	sig_handler(int signum);
-void	config_signals(void);
 void	flush_buffer(char *buffer, int *buffer_index);
+int		config_signals(void);
+int		config_signals(void);
 int		args_check(int argc, char **argv);
 int		main(int argc, char **argv);
 
